4085079cf70951962384455fe534a30b95848319
Luiz Fernando <luizfranca35mc@gmail.com>
2026-02-11 22:28:17 -0300

    colocando api de produtos com CORS

--- index.js
+++ index.js
@@ -2,41 +2,16 @@
 const express = require('express')
 const app = express()
 
+const cors = require('cors')
+app.use(cors())
+
 
 /* Indica que todas as requisicoes podem receber body em JSON. A partir disso, o Express aplica um JSON. parse para o conteudo recebido*/
 app.use(express.json())
 
 
-app.get('/', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.send('informatica')
-})
 
-
- //const lista_produtos = [
- //    {
- //        "titulo": "Red Nike",
-  //       "foto":"https://images.unsplash.com/photo-1542291026-7eec264c27ff?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w0NzEyNjZ8MHwxfHNlYXJjaHwxfHxzaG9lfGVufDB8MHx8fDE3MjEwNDEzNjd8MA&ixlib=rb-4.0.3&q=80&w=1080",
-   //     "descricao": "Tênis leve, com design versátil e acabamento moderno, perfeito para acompanhar sua rotina.",
-   //      "preco": 499.00,
-  //       "avaliacao": 5
-  //   },
-   //  {
-  //       "titulo": "Blue Nike",
-  //      "foto":"https://images.unsplash.com/photo-1515955656352-a1fa3ffcd111?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //       "descricao": "Modelo confortável, resistente e ideal para quem busca um visual urbano sem abrir mão do bem-estar.",
-  //       "preco": 699.00,
-  //      "avaliacao": 3
-  //   },
-   //  {
-  //      "titulo": "Black Nike",
-   //    "foto":"https://images.unsplash.com/photo-1643584549066-fc993fc9cb43?q=80&w=1080&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-  //       "descricao": "Tênis com ajuste confortável, visual clean e solado que garante estabilidade em cada passo.",
-   //      "preco": 799.00,
-    //     "avaliacao": 4
-   //  }
- //]
-
+ 
 // 1 - trazer apenas 12 primeiros produtos -- select * from produtos limit 12
 
 // 2 - trazer apenas produtos que começam com a letra A -- select * from produtos where  titulo like 'a%'
@@ -63,13 +38,7 @@ let conexao = mysql.createConnection({
 
 })
 
-app.get("/produtos", function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    conexao.query("select * from produtos", function (erro, lista_produtos, campos) {
-        console.log(lista_produtos)
-        res.send(lista_produtos)
-    })
-})
+
 
 
 conexao.connect(function (erro){
@@ -81,34 +50,25 @@ conexao.connect(function (erro){
     }
 })
 
-
-
-app.get('/', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.send('unidades')
+app.post("/produto/", function (req, res) {
+    const { titulo, preco, descricao, avaliacao, foto, categoria } = req.body;
+    conexao.query(`
+        INSERT INTO produtos(titulo, foto, descricao, preco, avaliacao, categoria)
+        values ('${titulo}' , '${descricao}', '${preco}', '${avaliacao}' , '${categoria}')`),
+        function (erro, resultado) {
+if (erro) {
+    res.json(erro);
+}
+res.send(resultado.insertid);
+        }
 })
 
-app.get("/unidades", function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    conexao.query("select * from produtos order by preco asc", function (erro, lista_unidades, campos) {
-        console.log(lista_unidades)
-        res.send(lista_unidades)
-    })
-})
 
-//Read by categoria - [GET] /produtos/:categoria
-app.get("/produtos/:categoria", function (req, res) {
-    res.setHeader("Access-Control-Allow-Origin", '*')
-    //pegamos a categoria que foi enviada na requisicao
-    const categoria = req.params.categoria
-    conexao.query('Select * from produtos where categoria=${categoria}', function (erro, dados, campos){
-        res.send(dados) })
-}),
 
-app.get('/produtos/:categoria/:ordem', function (req, res) {
-    res.setHeader('Access-Control-Allow-Origin', '*')
-    res.send('produtos')
-})
+
+
+
+
 
 
 
